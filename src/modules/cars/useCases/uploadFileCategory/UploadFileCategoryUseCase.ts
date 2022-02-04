e94330ee2c7240c897b8849344c2c9561653e500
kaiorr <kaio.rocharibeiro@gmail.com>
2022-02-03 21:37:21 -0300

    refactor: initial node stream  control

--- src/modules/cars/useCases/uploadFileCategory/UploadFileCategoryUseCase.ts
+++ src/modules/cars/useCases/uploadFileCategory/UploadFileCategoryUseCase.ts
@@ -1,6 +1,21 @@
+import fs from "fs";
+import { parse } from "csv-parse";
+
 class UploadFileCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+    const parser = parse();
+    const chunks = [];
+
+    stream.pipe(parser);
+
+    parser.on("readable", () => {
+      let chunk: any;
+      while ((chunk = parser.read())) {
+        chunks.push(chunk);
+        console.log(chunk);
+      }
+    });
   }
 }
 
