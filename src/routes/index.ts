190e71d22e6d6c0ff2418d70d92b6ccc75ec08da
kaiorr <kaio.rocharibeiro@gmail.com>
2022-02-07 21:36:36 -0300

    feat: authorize json module present route file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { categoriesRoute } from "./categories.route";
 import { specificationsRoute } from "./specifications.route";
+import swaggerUI from "swagger-ui-express";
+
+import swaggerSetup from "../docs/swagger.json";
 
 const router = Router();
+
+router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSetup));
 
 router.use(categoriesRoute);
 router.use(specificationsRoute);
